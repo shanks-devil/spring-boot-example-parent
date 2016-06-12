c44f2f2f363a49c527a52dde28c33d2e0c95d4f3
wang-liang <wang.liang@navercorp.com>
2016-06-12 23:43:32 +0800

    delete class

--- spring-boot-user/target/classes/import.sql
+++
@@ -1,3 +0,0 @@
-drop table if exists user_t;
-
-CREATE TABLE user_t (user_id INT NOT NULL AUTO_INCREMENT, user_name VARCHAR(100) NULL, user_email VARCHAR(100), user_phone VARCHAR(100),PRIMARY KEY (user_id));
